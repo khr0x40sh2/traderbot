876fe6fd310ac721de465143ed1fa2a76577f435
khr0x40sh2 <104953316+khr0x40sh2@users.noreply.github.com>
2022-05-05 09:07:00 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+### Webull login details - Warning, this is plain text and could expose your account. Use caution.
+
+symb='AAPL'
+
+login = '<email or phone number>'
+pw = '<password'
+trade_pin = '<trading pin>'
